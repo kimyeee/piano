1e22a0f31a90792e1cc8e81cbbbd51296393217c
kimyeee@163.com <kimyeee@163.com>
2018-03-02 11:13:01 +0800

    第一次提交

--- notice/views.py
+++ notice/views.py
@@ -1,11 +1,15 @@
 import time
+import requests
 import hashlib
-from django.shortcuts import render, HttpResponse ,redirect
+import json
+import re
+from django.shortcuts import render, HttpResponse, redirect
 from django.views.decorators.csrf import csrf_exempt
 
 wxtoken = 'weixin'
 my_wechat = 'JazzPiano_'
 msgtype = 'text'
+ROBOT_API = 'https://www.fedrobots.com/php/miji.php'
 
 notice = '''<xml>  
              <ToUserName><![CDATA[%s]]></ToUserName>  
@@ -14,6 +18,7 @@ notice = '''<xml>
              <MsgType><![CDATA[%s]]></MsgType>  
              <Content><![CDATA[%s]]></Content>  
              </xml>'''
+
 
 def wx_auth(request):
     signature = request.GET.get('signature')
@@ -26,10 +31,38 @@ def wx_auth(request):
 
     return signature == tmpstr
 
+
+def get_text(xml):
+    ret = re.findall('<Content><!\[CDATA\[.+\]\]', xml)[0][18:-2]
+    return ret
+
+
+def get_robot(request):
+    if request.method == 'GET':
+        text = '讲笑话'
+    else:
+        text = get_text(str(request.body))
+    if text:
+        data = {'action': 'getSend', 'problem_send': text}
+    else:
+        data = {'action': 'getSend', 'problem_send': text}
+    res = requests.post(ROBOT_API, data=data).text
+    msg = json.loads(res)
+    if msg.get('msg') == '获取成功':
+        refer_msg = msg.get('json').get('text')
+    else:
+        refer_msg = '机器人出错 !'
+    print(text)
+    print(refer_msg)
+    return refer_msg
+
+
 @csrf_exempt
 def piano(request):
     target_id = request.GET.get('openid')
     if wx_auth(request):
-        return HttpResponse(notice % (target_id, my_wechat, int(time.time()), msgtype, '说点啥呢。。。。'))
-    return redirect('http://mp.weixin.qq.com/s/Z1uMbza4BMUMnWcG-KS5Og')
-
+        # return HttpResponse(notice % (target_id, my_wechat, int(time.time()), msgtype, '说点啥呢。。。。'))
+        ret = get_robot(request)
+        return HttpResponse(notice % (target_id, my_wechat, int(time.time()), msgtype, ret))
+    return render(request,'index.html',{'msg':get_robot(request)})
+    # return redirect('http://mp.weixin.qq.com/s/Z1uMbza4BMUMnWcG-KS5Og')
