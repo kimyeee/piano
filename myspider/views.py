621c1265ebcea9bd840347e78500ec51aa610b59
kimyeee@163.com <kimyeee@163.com>
2018-03-23 18:46:17 +0800

    新增spider

--- myspider/views.py
+++ myspider/views.py
@@ -1,37 +1,59 @@
 from django.shortcuts import render
-
 import requests
 import threadpool
+import bs4
+import os
 
-ALL_PAGE = 515
 INIT_URL = 'http://bjmx.xdf.cn/guowaidaxue/_%s____'
 
+SETTINGS = {
+    'media_path': ''
+}
 
-class MySpider:
-    ALL_PAGE = 515
-    INIT_URL = 'http://bjmx.xdf.cn/guowaidaxue/_%s____'
 
-    def __init__(self,thread_num):
+class MyNetworkSpider:
+
+    def __init__(self, page_num, init_url, thread_num=50, parse_device='html.parser'):
+        self.page = page_num
+        self.init_url = init_url
         self.pool = threadpool.ThreadPool(thread_num)
+        self.parse = parse_device
         # requests = threadpool.makeRequests(get_page, )
 
-    def get_info(self):
+    def decoder(self, html, parse_device):
+        soup = bs4.BeautifulSoup(html, parse_device)
         return
 
-    def get_page(self, url):
+    def downloader(self, url):
         page_html = requests.get(url)
         return page_html
 
+    def geter(self):
+        re
+
+    def poster(self, url, data):
+        page_data = requests.post(url, data=data)
+        return page_data
+
     def thread_pool(self):
+        return
+
+    def storage(self, file, filename, is_bytes=None):
+        if is_bytes:
+            storage_mode = 'wb'
+        else:
+            storage_mode = 'w'
+        with open(os.path.join(SETTINGS['media_path'], filename), storage_mode) as file_device:
+            file_device.write(file)
         return
 
     def start(self):
         return
 
     def run(self):
-        self.get_page(INIT_URL)
+        self.downloader(INIT_URL)
 
 
 if __name__ == '__main__':
-    spider = MySpider(50)
+    spider = MyNetworkSpider(515, INIT_URL)
     spider.start()
