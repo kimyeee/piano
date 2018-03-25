4cf43bf4f8dba03ee604caff1a8c444311216516
kimyeee <kimyeee@163.com>
2018-03-26 00:32:35 +0800

    20180326

--- myspider/views.py
+++ myspider/views.py
@@ -1,42 +1,63 @@
 from django.shortcuts import render
+from django.db import models
+import time
 import requests
 import threadpool
 import bs4
 import os
 
+# from notice.models import University
+
+# import functools
+# from tornado import concurrent
+#
+# executor_pool = concurrent.futures.ThreadPoolExecutor()
+#
+#
+# def run_on_executor(fn):
+#     @functools.wraps(fn)
+#     def wrapper(*args, **kwargs):
+#         future = executor_pool.submit(fn, *args, **kwargs)
+#         return future
+#
+#     return wrapper
+
+
 INIT_URL = 'http://bjmx.xdf.cn/guowaidaxue/_%s____'
 
 SETTINGS = {
-    'media_path': './'
+    'media_path': r'C:\www\piano\notice'
 }
 
 
 class MyNetworkSpider:
 
-    def __init__(self, page_num, init_url, thread_num=50, parse_device='html.parser'):
+    def __init__(self, page_num, init_url, thread_num=100, parse_device='html.parser'):
         self.page = page_num
         self.init_url = init_url
         self.pool = threadpool.ThreadPool(thread_num)
-        self.parse = parse_device
-        # requests = threadpool.makeRequests(get_page, )
+        self.parse_device = parse_device
 
-    def decoder(self, html, parse_device):
-        soup = bs4.BeautifulSoup(html, parse_device)
+    def decoder(self, html):
+        soup = bs4.BeautifulSoup(html, self.parse_device)
         return soup
 
     def downloader(self, url):
-        page_html = requests.get(url)
-        return page_html
-
-    def geter(self,url):
         file = requests.get(url)
         return file
+
+    def geter(self, url):
+        page_html = requests.get(url)
+        return page_html
 
     def poster(self, url, data):
         page_data = requests.post(url, data=data)
         return page_data
 
-    def thread_pool(self):
+    def thread_pool(self, function, queryset):
+        tasks = threadpool.makeRequests(function, queryset)
+        for task in tasks:
+            self.pool.putRequest(task)
         return
 
     def storage(self, file, filename, is_bytes=None):
@@ -44,22 +65,65 @@ class MyNetworkSpider:
             storage_mode = 'wb'
         else:
             storage_mode = 'w'
-        with open(os.path.join(SETTINGS['media_path'], filename), storage_mode) as file_device:
+        with open(os.path.join(SETTINGS.get('media_path', './'), filename), storage_mode) as file_device:
             file_device.write(file)
         return
 
-    def start(self):
-        self.test()
-        return
-
-    def test(self):
-        print(os.path.join(SETTINGS['media_path'], 'test.jpg'))
-        return
-
-    def run(self):
-        self.downloader(INIT_URL)
+    # def start(self):
+    #     self.test()
+    #     return
 
 
-if __name__ == '__main__':
-    spider = MyNetworkSpider(515, INIT_URL)
-    spider.start()
+#     def test(self):
+#         for i in range(1, 11):
+#             print(i, '========================')
+#             self.thread_pool(aaa, [i * j for j in range(1, 11)])
+#         self.pool.wait()
+#         return
+#
+#     def run(self):
+#         self.downloader(INIT_URL)
+#
+#
+# def aaa(a):
+#     time.sleep(1)
+#     print(a)
+#
+#
+# if __name__ == '__main__':
+#     spider = MyNetworkSpider(515, INIT_URL)
+#     spider.start()
+
+class GetUniversitySpider(MyNetworkSpider):
+
+    def decoder(self, html):
+        soup = bs4.BeautifulSoup(html, self.parse_device)
+        ret = soup.find_all('div', class_='block-g')
+        university_dict = []
+        for uni in ret:
+            img = uni.find('img')['src']
+            name = uni.find_all('a')[1].contents[0]
+            e_name = uni.find_all('a')[1].contents[3]
+            school_type = uni.find('p', class_='p1').get_text().split('\n')[2]
+            address = uni.find('p', class_='p2').get_text().split('\n')[2]
+            TOEFL_score, SAT_score, instructions = uni.find('div', class_='zklt').find_all('p')
+            university_dict.append({
+                'c_name': name,
+                'e_name': e_name,
+                'school_type': school_type,
+                'address': address,
+                'TOEFL_score': TOEFL_score.find('span').get_text(),
+                'SAT_score': SAT_score.find('span').get_text(),
+                'instructions': instructions.find('span').get_text(),
+                'img':img
+            })
+
+        return university_dict
+
+
+sss = GetUniversitySpider(515, INIT_URL, 10)
+html = open('page_1.html', 'rb')
+queyset = sss.decoder(html)
+for i in queyset:
+    print('---------------')
+    print(i)
--- notice/models.py
+++ notice/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class University(models.Model):
+    c_name = models.CharField('中文名',max_length=100)
+    e_name = models.CharField('英文名',max_length=100)
+    school_type = models.CharField('学校类型',max_length=100)
+    address = models.CharField('学校地理位置',max_length=100)
+    TOEFL_score = models.CharField('托福成绩',max_length=100)
+    SAT_score = models.CharField('SAT成绩',max_length=100)
+    instructions = models.TextField('学校简介')
