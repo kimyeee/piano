3d17b445efe9786f0884d3fcb0ae834dd01663fa
kimyeee@163.com <kimyeee@163.com>
2018-03-26 20:25:21 +0800

    新增spider

--- myspider/tests.py
+++ myspider/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+import requests
+
+ret = requests.get('http://bjmx.xdf.cn/guowaidaxue/_1_caababc8f15c44f28daa4d6c733e64ad___')
+open('page_4_1.html','wb').write(ret.content)
--- myspider/views.py
+++ myspider/views.py
@@ -5,6 +5,10 @@ import requests
 import threadpool
 import bs4
 import os
+import random
+import xlwt
+import xlrd
+from xlutils.copy import copy
 
 # from notice.models import University
 
@@ -28,12 +32,13 @@ INIT_URL = 'http://bjmx.xdf.cn/guowaidaxue/_%s____'
 SETTINGS = {
     'media_path': r'C:\www\piano\notice'
 }
+s_time = time.time()
 
 
 class MyNetworkSpider:
 
-    def __init__(self, page_num, init_url, thread_num=100, parse_device='html.parser'):
-        self.page = page_num
+    def __init__(self, page_num, init_url, thread_num=1, parse_device='html.parser'):
+        self.page_num = page_num
         self.init_url = init_url
         self.pool = threadpool.ThreadPool(thread_num)
         self.parse_device = parse_device
@@ -58,6 +63,7 @@ class MyNetworkSpider:
         tasks = threadpool.makeRequests(function, queryset)
         for task in tasks:
             self.pool.putRequest(task)
+        # self.pool.wait()
         return
 
     def storage(self, file, filename, is_bytes=None):
@@ -69,61 +75,132 @@ class MyNetworkSpider:
             file_device.write(file)
         return
 
-    # def start(self):
-    #     self.test()
-    #     return
-
-
-#     def test(self):
-#         for i in range(1, 11):
-#             print(i, '========================')
-#             self.thread_pool(aaa, [i * j for j in range(1, 11)])
-#         self.pool.wait()
-#         return
-#
-#     def run(self):
-#         self.downloader(INIT_URL)
-#
-#
-# def aaa(a):
-#     time.sleep(1)
-#     print(a)
-#
-#
-# if __name__ == '__main__':
-#     spider = MyNetworkSpider(515, INIT_URL)
-#     spider.start()
 
 class GetUniversitySpider(MyNetworkSpider):
+    SCORES_TYPE = {
+        '托福成绩: ': 'TOEFL_score',
+        'SAT成绩: ': 'SAT_score',
+        '雅思成绩: ': 'IELTS_score',
+        'GMAT成绩: ': 'GMAT_score',
+        '简介:': 'instructions',
+    }
+
+    def __init__(self, page_num, init_url, count=0):
+        super().__init__(page_num, init_url)
+        self.count = count
+
+        self.book = xlwt.Workbook(encoding='utf-8', style_compression=0)
+        if os.path.isfile('test.xls'):
+            count_book = xlrd.open_workbook('test.xls')
+            count_sheet = count_book.sheet_by_index(0)
+            self.count = count_sheet.nrows + 1
+            self.row = count_sheet.nrows
+            self.book = copy(count_book)
+            self.sheet = self.book.get_sheet(0)
+        else:
+            self.sheet = self.book.add_sheet('test', cell_overwrite_ok=True)
+            self.book_init()
+            self.row = self.count
+
+    def book_init(self):
+        print(__name__)
+        self.sheet.write(0, 0, '中文名')
+        self.sheet.write(0, 1, '英文名')
+        self.sheet.write(0, 2, '学校类型')
+        self.sheet.write(0, 3, '学校地址')
+        self.sheet.write(0, 4, '托福成绩')
+        self.sheet.write(0, 5, '雅思成绩')
+        self.sheet.write(0, 6, 'SAT成绩')
+        self.sheet.write(0, 7, 'GMAT成绩')
+        self.sheet.write(0, 8, '学校简介')
+        self.sheet.write(0, 9, '学校校徽')
+        self.sheet.write(0, 10, '详情链接')
+        self.count = 1
 
     def decoder(self, html):
         soup = bs4.BeautifulSoup(html, self.parse_device)
         ret = soup.find_all('div', class_='block-g')
-        university_dict = []
+        university_list = []
+        print(len(ret))
         for uni in ret:
             img = uni.find('img')['src']
             name = uni.find_all('a')[1].contents[0]
-            e_name = uni.find_all('a')[1].contents[3]
+            try:
+                e_name = uni.find_all('a')[1].contents[3]
+            except:
+                e_name = uni.find_all('a')[1].contents[2]
             school_type = uni.find('p', class_='p1').get_text().split('\n')[2]
             address = uni.find('p', class_='p2').get_text().split('\n')[2]
-            TOEFL_score, SAT_score, instructions = uni.find('div', class_='zklt').find_all('p')
-            university_dict.append({
-                'c_name': name,
-                'e_name': e_name,
+            # TOEFL_score, SAT_score, instructions = uni.find('div', class_='zklt').find_all('p')
+            scores_list = uni.find('div', class_='zklt').find_all('p')
+            scores_dict = {}
+            for item in scores_list:
+                key = item.find('b').get_text()
+                text = item.find('span').get_text()
+                scores_dict[key] = text
+
+            university = {
+                'c_name': str(name),
+                'e_name': str(e_name),
                 'school_type': school_type,
                 'address': address,
-                'TOEFL_score': TOEFL_score.find('span').get_text(),
-                'SAT_score': SAT_score.find('span').get_text(),
-                'instructions': instructions.find('span').get_text(),
-                'img':img
-            })
+                # 'TOEFL_score': TOEFL_score.find('span').get_text(),
+                # 'SAT_score': SAT_score.find('span').get_text(),
+                # 'instructions': instructions.find('span').get_text(),
+                'badge_img_id': self.count,
+                'badge_img_url': img,
+            }
+            university['scores_dict'] = scores_dict
+            university_list.append(university)
+            self.count += 1
 
-        return university_dict
+        return university_list
+
+    def get_init_url(self):
+        request_list = []
+        for page in range(1, self.page_num - 510):
+            url = self.init_url % page
+            request_list.append(url)
+        return request_list
+
+    def run(self, abc):
+        wait_time = random.uniform(0, 9)
+        # time.sleep(wait_time)
+        html = open('page_4_1.html', 'rb')
+        queryset = self.decoder(html)
+        # for i in queryset:
+        #     print('---------------')
+        #     print(i, ':')
+
+        self.storage(queryset, self.count)
+
+    def storage(self, file, filename, is_bytes=None):
+
+        for university in file:
+            self.sheet.write(self.row, 0, university['c_name'])
+            self.sheet.write(self.row, 1, university['e_name'])
+            self.sheet.write(self.row, 2, university['school_type'])
+            self.sheet.write(self.row, 3, university['address'])
+            # self.sheet.write(self.row, 4, university['TOEFL_score'])
+            # self.sheet.write(self.row, 5, university['SAT_score'])
+            # self.sheet.write(self.row, 6, university['instructions'])
+            self.sheet.write(self.row, 6, university['scores_dict'])
+            self.sheet.write(self.row, 7, university['badge_img_id'])
+            self.sheet.write(self.row, 8, university['badge_img_url'])
+            self.row += 1
+        print(self.count)
 
 
-sss = GetUniversitySpider(515, INIT_URL, 10)
-html = open('page_1.html', 'rb')
-queyset = sss.decoder(html)
-for i in queyset:
-    print('---------------')
-    print(i)
+spider = GetUniversitySpider(515, INIT_URL)
+
+url_list = spider.get_init_url()
+queryset = spider.thread_pool(spider.run, url_list)
+
+# for i in queryset[0]:
+# print('---------------')
+# print(i, ':', queryset[0][i])
+
+spider.pool.wait()
+spider.book.save('test.xls')
+run_time = time.time() - s_time
+print(run_time)
